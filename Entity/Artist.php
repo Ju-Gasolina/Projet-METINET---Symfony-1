9628ed312156536ee4583046b5d5a3c962a5b117
Justin <>
2022-11-07 17:41:35 +0100

    modify style favorite

--- Entity/Artist.php
+++ Entity/Artist.php
@@ -105,12 +105,9 @@ class Artist extends Model
             <div class='card'>
                 <img class='card-img-top' src=".$this->getPicture()." alt='Photo de ".$this->getName()."'>
                 <div class='card-body'>
-                    <p class='card-title h5 text-center'>".$this->getName()."</p>
-                    <p class='card-text'>".$gendersText."</p>
-                    <div class='text-center'>
-                        <a href=".$linkText." class='btn btn-secondary text-white' title='Cliquer pour voir la page Spotify de cet artiste' target='_blank'>Page Spotify de l'artiste</a>
-                        <a href='/album/list/".$this->getId()."' class='btn btn-secondary text-white' title='Cliquer pour voir les albums de cet artiste' target='_blank'>Albums de l'artiste</a>
-                        <form action='/SearchArtist/addFavorite/".$this->getId()."' method='post'>
+                    <div class='d-flex align-items-center justify-content-center'>
+                        <p class='card-title h5 text-center px-1'>".$this->getName()."</p>
+                        <form action='/SearchArtist/addFavorite/".$this->getId()."' method='post' class='px-1'>
                             <input type='hidden' name='idSpotify' value='".$this->getId()."'>
                             <input type='hidden' name='name' value='".$this->getName()."'>
                             <input type='hidden' name='followers' value='".$this->getFollowers()."'>
@@ -118,8 +115,14 @@ class Artist extends Model
                             <input type='hidden' name='link' value='".$this->getLink()."'>
                             <input type='hidden' name='pictures' value='".$this->getPicture()."'>
                             
-                            <button class='btn btn-primary text-white' type='submit' title='Cliquer pour cet artiste à vos favoris'>Ajouter à vos favoris</button>
+                            <button class='icon icon_star' type='submit' title='Cliquer pour ajouter cet artiste à vos favoris'></button>
                         </form>
+                    </div>
+                    
+                    <p class='card-text'>".$gendersText."</p>
+                    <div class='text-center'>
+                        <a href=".$linkText." class='btn btn-secondary text-white' title='Cliquer pour voir la page Spotify de cet artiste' target='_blank'>Page Spotify de l'artiste</a>
+                        <a href='/album/list/".$this->getId()."' class='btn btn-secondary text-white' title='Cliquer pour voir les albums de cet artiste' target='_blank'>Albums de l'artiste</a>
                     </div>
                 </div>
             </div>
